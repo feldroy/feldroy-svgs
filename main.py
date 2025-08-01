d2c56bac1b1f330f896b1d99d57bc3b2f5cf9c0a
Audrey M. Roy Greenfeld <audrey@feldroy.com>
2025-08-01 19:50:14 +0800

    Implement navigation and layout functions, add usage policy page, and update dependencies

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import air
+from air_markdown import Markdown
+from fastapi import HTTPException
 from fastapi import FastAPI
+from pathlib import Path
 
 app = air.Air()
 api = FastAPI()
 
 app.mount("/static", air.StaticFiles(directory="static"), name="static")
 
+
+def nav():
+    return air.Nav(
+        air.A("Home", href="/"),
+        air.A("Usage Policy", href="/usage-policy"),
+    )
+
+
 @app.page
 def index():
     return air.layouts.mvpcss(
+        nav(),
         air.Title("Feldroy SVGs"),
         air.H1("The Feldroy logo as SVG"),
         air.P("This page contains the Feldroy logo in SVG format."),
@@ -23,6 +35,34 @@ def index():
             ),
         )
     )
+
+
+def layout(request: air.Request, *content):
+    if not isinstance(request, air.Request):
+        raise Exception('First arg of layout needs to be an air.Request')
+    return air.layouts.mvpcss(
+        nav(),
+        *content
+    )
+
+
+@app.get('/{slug:path}')
+def mdpage(request: air.Request, slug: str):
+    path = Path(f"pages/{slug}.md")
+    if path.exists():
+        text = path.read_text()
+        # TODO add fetching of page title from first H1 tag
+        return layout(
+            request, Markdown(text)
+        )
+    path = Path(f"pages/{slug}.py")
+    if path.exists():
+        module_name = f'pages.{slug.replace('/', '.')}'     
+        mod = importlib.import_module(module_name)
+        return layout(
+            request, mod.render(request)
+        )
+    raise HTTPException(status_code=404)
 
 
 if __name__ == "__main__":
