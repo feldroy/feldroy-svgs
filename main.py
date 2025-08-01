9d57638616e9d5e24a866da2f1ddec4887d5e134
Audrey M. Roy Greenfeld <audrey@feldroy.com>
2025-08-01 16:17:45 +0800

    Add a quick Air site

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from feldroy-svgs!")
+import air
+from fastapi import FastAPI
+
+app = air.Air()
+api = FastAPI()
+
+@app.page
+def index():
+    return air.layouts.mvpcss(
+        air.Title("Feldroy SVGs"),
+        air.H1("The Feldroy logo and other SVGs"),
+        air.P("This page contains the Feldroy logo in SVG format, along with other related SVGs."),
+    )
 
 
 if __name__ == "__main__":
-    main()
+    print("Run this with fastapi dev")
