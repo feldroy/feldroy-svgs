f91410854a22d7a6e99de6cab91a9d9053f6b0fc
Audrey M. Roy Greenfeld <audrey@feldroy.com>
2025-08-01 16:28:35 +0800

    Mount static svgs in /static, show in Air app

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from fastapi import FastAPI
 app = air.Air()
 api = FastAPI()
 
+app.mount("/static", air.StaticFiles(directory="static"), name="static")
+
 @app.page
 def index():
     return air.layouts.mvpcss(
         air.Title("Feldroy SVGs"),
-        air.H1("The Feldroy logo and other SVGs"),
-        air.P("This page contains the Feldroy logo in SVG format, along with other related SVGs."),
+        air.H1("The Feldroy logo as SVG"),
+        air.P("This page contains the Feldroy logo in SVG format."),
+        air.Img(src="/static/feldroy-logo-square.svg", alt="Feldroy Logo", width=200, height=200),
     )
 
 
